4eaeef529fc3c8d344809a86ec53a790317d1e41
bedlinger <bedlinger@student.tgm.ac.at>
2024-04-30 16:03:41 +0200

    added quasar to project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
-createApp(App).mount("#app");
+createApp(App).use(Quasar, quasarUserOptions).mount("#app");
