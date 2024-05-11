460bf942a98f15dbdad0b39cab5b5fbcbdd7b7fd
bedlinger <bedlinger@student.tgm.ac.at>
2024-05-11 22:38:28 +0200

    Start with App.vue, also center and fixed size window

--- src/quasar-user-options.js
+++ src/quasar-user-options.js
@@ -4,7 +4,20 @@ import '@quasar/extras/material-icons/material-icons.css'
 
 // To be used on app.use(Quasar, { ... })
 export default {
-  config: {},
-  plugins: {
-  }
-}
+  config: {
+    brand: {
+      primary: "#1976d2",
+      secondary: "#26A69A",
+      accent: "#9C27B0",
+
+      dark: "#1d1d1d",
+      "dark-page": "#121212",
+
+      positive: "#21BA45",
+      negative: "#C10015",
+      info: "#31CCEC",
+      warning: "#F2C037"
+    }
+  },
+  plugins: {},
+};
