19e23ef7e3ea1f8751737035a3afd1cbf3df9741
bedlinger <bedlinger@student.tgm.ac.at>
2024-05-16 17:57:07 +0200

    Add search and list of apps to launch

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,57 @@
 
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_installed_apps, launch_app])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .unwrap_or_else(|e| eprintln!("Error while running tauri application: {}", e));
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct App {
+    name: String,
+    path: String
+}
+
+#[tauri::command]
+fn get_installed_apps() -> Vec<App> {
+    let mut apps = vec![];
+    let paths = vec![
+        "C:\\ProgramData\\Microsoft\\Windows\\Start Menu"
+    ];
+    for path in paths {
+        get_apps_from_dir(&path, &mut apps);
+    }
+    apps
+}
+
+fn get_apps_from_dir(path: &str, apps: &mut Vec<App>) {
+    if let Ok(entries) = std::fs::read_dir(path) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                if let Ok(metadata) = entry.metadata() {
+                    if metadata.is_dir() {
+                        get_apps_from_dir(&entry.path().to_string_lossy().to_string(), apps);
+                    } else if entry.path().extension().and_then(std::ffi::OsStr::to_str) == Some("lnk") {
+                        if let Ok(name) = entry.file_name().into_string() {
+                            apps.push(App {
+                                name: name,
+                                path: entry.path().to_string_lossy().to_string()
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[tauri::command]
+fn launch_app(app: App) {
+    println!("Launching app: {}", app.path);
+    // Verknüpfung öffnen
+    if let Ok(_) = std::process::Command::new("explorer")
+        .arg(app.path)
+        .spawn() {
+        return;
+    }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-    <q-toolbar class="bg-grey-9 text-white">
-        <q-toolbar-title>
-            App-Launcher
-        </q-toolbar-title>
-        <q-btn flat round dense @click="showSettings = true">
-            <q-icon name="more_vert" />
-        </q-btn>
-    </q-toolbar>
+    <q-layout view="hHh lpR fFf">
+        <q-header elevated class="bg-primary text-white">
+            <q-toolbar>
+                <q-toolbar-title>
+                    App-Launcher
+                </q-toolbar-title>
+                <q-space />
+                <q-btn flat round dense icon="more_vert" />
+            </q-toolbar>
+        </q-header>
+        <q-page-container>
+            <q-page class="q-pa-md bg-white">
+                <q-input v-model="search" placeholder="Search" filled dense debounce="300" />
+                <q-list bordered separator>
+                    <q-item v-for="app in apps" :key="app.path.toString" clickable @click="launchApp(app)">
+                        <q-item-section avatar>
+                            <q-icon name="apps" />
+                        </q-item-section>
+                        <q-item-section>
+                            {{ app.name }}
+                        </q-item-section>
+                    </q-item>
+                </q-list>
+            </q-page>
+        </q-page-container>
+    </q-layout>
 </template>
 
 <script lang="ts">
 import { appWindow } from '@tauri-apps/api/window'
+import { invoke } from '@tauri-apps/api'
+
+interface App {
+    name: String,
+    path: String
+}
 
 export default {
     name: "App",
     data() {
         return {
-            showSettings: false
+            apps: [] as App[],
+            search: ''
         }
     },
     methods: {
+        async getInstalledApps(searchTerm: string = '') {
+            try {
+                this.apps = await invoke('get_installed_apps', { searchTerm })
+            } catch (error) {
+                console.error("Error retrieving installed apps:", error)
+            }
+        },
+        launchApp(app: App) {
+            invoke('launch_app', { app })
+                .catch(console.error)
+                .then(_onfullfilled => {
+                    // appWindow.close()
+                })
+        }
+    },
+    computed: {
+    },
+    watch: {
+        search(newSearch: string) {
+            this.getInstalledApps(newSearch)
+        }
     },
     mounted() {
-        appWindow.center().catch(console.error)
+        appWindow.center()
+            .catch(console.error)
     }
 }
 </script>
